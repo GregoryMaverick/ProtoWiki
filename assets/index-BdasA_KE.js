1a57bd2b5317841eec4eb8f0fdd5bbb341145473
GregoryMaverick <GregoryMaverick@users.noreply.github.com>
2026-07-24 13:34:07 +0000

    Deploying to gh-pages from @ GregoryMaverick/ProtoWiki@1241282dc6d5dbeceee5743346ed7872976a399a 🚀

---
+++ assets/index-BdasA_KE.js
@@ -0,0 +1,1 @@
+import{P as a}from"./PlainWrapper-Ba_D9bhv.js";import{d as r,c as t,w as p,o as n,a as o}from"./index-jo7Y0qiT.js";const d=r({__name:"index",setup(s){return(i,e)=>(n(),t(a,{heading:"Plain wrapper"},{default:p(()=>[...e[0]||(e[0]=[o("p",null,"Starter template — empty plain wrapper.",-1)])]),_:1}))}});export{d as default};
